066454d52dcbd3717ee2eec9be9c9b3f4df88146
Aries Penetrante <penetrantearies@gmail.com>
2021-01-25 18:33:30 +0200

    Add project files.

---
+++ WriteToFileReadToFile/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WriteToFileReadToFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readBtn = new System.Windows.Forms.Button();
+            this.writeBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.coptBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(12, 401);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(75, 23);
+            this.readBtn.TabIndex = 0;
+            this.readBtn.Text = "Read";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // writeBtn
+            // 
+            this.writeBtn.Location = new System.Drawing.Point(134, 401);
+            this.writeBtn.Name = "writeBtn";
+            this.writeBtn.Size = new System.Drawing.Size(75, 23);
+            this.writeBtn.TabIndex = 0;
+            this.writeBtn.Text = "Write";
+            this.writeBtn.UseVisualStyleBackColor = true;
+            this.writeBtn.Click += new System.EventHandler(this.writeBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(665, 401);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 0;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(23, 59);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(717, 309);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
+            // 
+            // coptBtn
+            // 
+            this.coptBtn.Location = new System.Drawing.Point(258, 401);
+            this.coptBtn.Name = "coptBtn";
+            this.coptBtn.Size = new System.Drawing.Size(75, 23);
+            this.coptBtn.TabIndex = 0;
+            this.coptBtn.Text = "Copy";
+            this.coptBtn.UseVisualStyleBackColor = true;
+            this.coptBtn.Click += new System.EventHandler(this.coptBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(378, 401);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 0;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.coptBtn);
+            this.Controls.Add(this.writeBtn);
+            this.Controls.Add(this.readBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.Button writeBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button coptBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ WriteToFileReadToFile/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WriteToFileReadToFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeBtn_Click(object sender, EventArgs e)
+        {
+
+            writetofile();
+
+
+
+        }
+
+
+
+        private void writetofile()
+
+
+        {
+
+            /// Start of Writing 
+            string filePath = @"C:\TestFile\Testfile.txt";
+            string content = outputTextBox.Text;
+
+            using (StreamWriter outputFile = new StreamWriter(filePath))
+            {
+                outputFile.WriteLine(content);
+            }
+            ///end of writing
+
+
+
+
+        }
+        /// end of writetofile()
+        /// 
+
+        private void readtofile()
+
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.ShowDialog();
+
+            string readfile = File.ReadAllText(dialog.FileName);
+
+            outputTextBox.Text = readfile;
+
+
+
+
+
+
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+
+            readtofile();
+
+        }
+
+        private void coptBtn_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(outputTextBox.Text);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = string.Empty;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+
+        ///end of readtofile()
+
+
+
+    }
+
+}
